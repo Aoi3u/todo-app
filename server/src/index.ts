911f02a8b7738584015cb81ceb887436cc3d4a7f
miu3u <miu.blue3@gmail.com>
2025-08-05 12:51:05 +0900

    feat: prisma and GET

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello')
+import { PrismaClient } from '../generated/prisma';
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+const prisma = new PrismaClient;
+
+app.get("/allTodos", async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany();
+  return res.json(allTodos);
+
+});
+
+app.listen(PORT, () => console.log("server is running"));
